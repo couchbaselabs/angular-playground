93917dd175cd6a3585231b8411f17fa71c161461
Matthew Dawber <matthew.dawber@couchbase.com>
2022-03-07 15:34:43 +0000

    Add 'projects' component. Move styles to app.component.scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'projects', component: ProjectsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddProjectComponent } from './add-project/add-project.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    AddProjectComponent
+    AddProjectComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
