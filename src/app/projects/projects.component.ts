f912a4b41d115068e39683889e0d3505eb3cbf49
lukaisthewolf <raluca.lupu@couchbase.com>
2022-03-11 17:28:51 +0000

    Add spinner while adding a project. Add check/error icons for Add project response.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,12 @@
 import {ChangeDetectionStrategy, Component, OnInit, OnDestroy} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {BehaviorSubject, Subject, takeUntil} from 'rxjs';
+import {
+  BehaviorSubject,
+  Subject,
+  takeUntil,
+  tap,
+  timer
+} from 'rxjs';
 import {ProjectsService} from "./projects.service";
 
 type Projects = Array<any> | null;
@@ -17,21 +23,27 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   destroy$: Subject<boolean> = new Subject<boolean>();
   projects$: BehaviorSubject<Projects> = new BehaviorSubject<Projects>(null);
   projectName: string = '';
+  fieldIsLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  success$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  error$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private projectsService: ProjectsService) {
     this.projectsService = projectsService;
 
-    this.projectsService.getProjectsPoller()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(projects => {
-      this.projects$.next(JSON.parse(projects.toString()).data);
-    });
+    this.projectsService
+      .getProjectsPoller()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(projects => {
+        this.projects$.next(JSON.parse(projects.toString()).data);
+      });
   }
 
   ngOnInit() {
     this.clickAddProject$
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(this.addProject.bind(this));
+      .pipe(
+        tap(() => this.fieldIsLoading$.next(true)),
+        takeUntil(this.destroy$))
+      .subscribe(this.addProject.bind(this));
   }
 
   ngOnDestroy() {
@@ -40,14 +52,27 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   }
 
   addProject(addProjectForm: NgForm) {
-    this.projectsService.addProject(addProjectForm.value.projectName)
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(() => this.projectsService.updateProject.next(true));
+    this.projectsService
+      .addProject(addProjectForm.value.projectName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.projectsService.updateProject.next(true);
+        this.fieldIsLoading$.next(false);
+        this.success$.next(true);
+        this.error$.next(false);
+        timer(2000).pipe(takeUntil(this.destroy$)).subscribe(() => this.success$.next(false));
+      },() => {
+        this.fieldIsLoading$.next(false);
+        this.error$.next(true);
+        this.success$.next(false);
+        timer(2000).pipe(takeUntil(this.destroy$)).subscribe(() => this.error$.next(false));
+      });
   }
 
   deleteProject(projectId: string) {
-    this.projectsService.deleteProject(projectId)
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(() => this.projectsService.updateProject.next(true));
+    this.projectsService
+      .deleteProject(projectId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.projectsService.updateProject.next(true));
   }
 }
