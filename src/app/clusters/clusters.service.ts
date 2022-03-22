3d713b9fa5a674f216383d488f36544fe24b2f68
Matthew Dawber <matthew.dawber@couchbase.com>
2022-03-22 17:00:32 +0000

    Mock Add Cluster request

--- src/app/clusters/clusters.service.ts
+++ src/app/clusters/clusters.service.ts
@@ -10,8 +10,54 @@ export class ClustersService {
   }
 
   addCluster(clusterName: string) {
-    return this.httpClient.post('https://cloudapi.cloud.couchbase.com/v3/clusters', { clusterName: clusterName });
+    return this.httpClient.post('https://cloudapi.cloud.couchbase.com/v3/clusters',
+                                {
+                                  "environment": "hosted",
+                                  "servers": [
+                                    {
+                                      "compute": "m5.xlarge",
+                                      "size": 4,
+                                      "services": [
+                                        "data",
+                                      ],
+                                      "storage": {
+                                        "size": 50,
+                                        "IOPS": 3000,
+                                        "type": "GP3"
+                                      }
+                                    },
+                                    {
+                                      "compute": "m5.xlarge",
+                                      "size": 4,
+                                      "services": [
+                                        "index"
+                                      ],
+                                      "storage": {
+                                        "size": 50,
+                                        "IOPS": 3000,
+                                        "type": "GP3"
+                                      }
+                                    }
+                                  ],
+                                  "supportPackage": {
+                                    "timezone": "IST",
+                                    "type": "DeveloperPro"
+                                  },
+                                  "clusterName": "UI Test Cluster",
+                                  "description": "Example description of a G2 cluster",
+                                  "place": {
+                                    "singleAZ": true,
+                                    "hosted": {
+                                      "provider": "aws",
+                                      "CIDR": "10.2.0.0/20",
+                                      "region": "us-west-2"
+                                    }
+                                  },
+                                  "projectId": "5de5f32b-866c-4a90-bafd-8bdaebb842cd"
+                                }
+                               );
   }
+
 
   getClusters() {
     return this.httpClient.get('https://cloudapi.cloud.couchbase.com/v3/clusters');
