39b280e5e76da2cd9f134d89c63ac66ad3921e85
Matthew Dawber <matthew.dawber@couchbase.com>
2022-03-28 09:55:18 +0100

    Add preload.js script to start go server

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
 const { app, BrowserWindow } = require("electron");
-const spawn = require("child_process").spawn;
 const path = require("path");
 
 let mainWindow;
 
-// Exit if we don't have the required ENVIORNMENT variables.
-if (!process.env.BASE_URL) {
-  console.log("Please set BASE_URL environment variable");
-  process.exit(1)
-}
-if (!process.env.SECRET_KEY) {
-  console.log("Please set SECRET_KEY environment variable");
-  process.exit(1)
-}
-if (!process.env.ACCESS_KEY) {
-  console.log("Please set ACCESS_KEY environment variable");
-  process.exit(1)
-}
+const IS_DEV = process.env.NODE_ENV === "development";
+const SERVER_DIR = IS_DEV ? __dirname : process.resourcesPath;
+const LOAD_PATH = IS_DEV ? "http://localhost:4200" : "dist/angular-playground/index.html";
 
-// Disable CORS for development mode.
-if (isDev()) {
-  app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors');
-}
+/** Emits when Electron is initializing. */
+app.on("will-finish-launching", () => {
 
-function isDev() {
-  return process.env['NODE_ENV'] === "development";
-}
+  /** Exit if required environment variables not present. */
+  ["BASE_URL", "SECRET_KEY", "ACCESS_KEY"].forEach(checkEnvVariablePresent);
 
+  /** Disable CORS for development mode during startup. */
+  if (IS_DEV) disableCors();
+});
+
+/** Create Window when Electron has finished initializing. */
+app.on("ready", () => {
+  createWindow();
+});
+
+/** Quit when all windows have been closed. */
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+/**
+ * Creates and returns a new BrowserWindow instance.
+ * @property {integer} x - Window x position.
+ * @property {integer} y - Window y position.
+ * @property {integer} width - Window width.
+ * @property {integer} height - Window height.
+ * @property {boolean} webPreferences.nodeIntegration - Crucial for security.
+ * @property {boolean} webPreferences.webSecurity - Crucial for security.
+ * @property {boolean} webPreferences.enableRemoteModule - Crucial for security.
+ * @property {function} webPreferences.preload - Runs preload script.
+ *
+ * @returns {BrowserWindow}
+ */
 function createWindow() {
   mainWindow = new BrowserWindow({
     x: 0,
@@ -34,37 +47,30 @@ function createWindow() {
     width: 1800,
     height: 1400,
     webPreferences: {
-      webSecurity: !isDev(),
-      nodeIntegration: true,
+      webSecurity: !IS_DEV,
+      nodeIntegration: false,
+      enableRemoteModule: false,
+      preload: path.resolve(SERVER_DIR, "preload.js")
     },
   });
 
-  if (isDev()) {
-    mainWindow.loadURL("http://localhost:4200");
-  } else {
-    mainWindow.loadFile("dist/angular-playground/index.html");
+  mainWindow.loadURL(LOAD_PATH);
+}
+
+/** Disables CORS. */
+function disableCors () {
+  app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors');
+}
+
+/**
+ * Checks presence of environment variable.
+ * Exits if not present.
+ *
+ * @param {string} key - Environment variable name.
+ */
+function checkEnvVariablePresent(key) {
+  if (!process.env[key]) {
+    console.log(`${key} environment variable is not set`);
+    process.exit(1)
   }
-
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
 }
-
-function spawnServer() {
-  let directory = isDev() ? __dirname : process.resourcesPath;
-
-  spawn(path.join(directory, "backend/main"));
-}
-
-app.on("ready", () => {
-  createWindow();
-  spawnServer();
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
-
-app.on("activate", () => {
-  if (mainWindow === null) createWindow();
-});
